7601b8f00efb362a86e005aba220d195b8f5b045
xp2008 <hslzyao@163.com>
2024-07-24 17:09:48 +0800

    element-ui引入自动按需引入

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+
+//elelments-plus按需引入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    // 实际的路径转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
