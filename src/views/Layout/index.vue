518cd5792973f5ddf33b526e5418ae090c51a4fc
xp2008 <hslzyao@163.com>
2024-07-25 14:48:58 +0800

    layout-静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 <template>
-    <div>首页</div>
+    <LayoutNav />
+    <LayoutHeader />
     <!--二级路由出口-->
     <RouterView />
+    <LayoutFooter />
 </template>
