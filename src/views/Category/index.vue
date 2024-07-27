97310ed39f591e69647436fa226782c12e32eacb
xp2008 <hslzyao@163.com>
2024-07-27 20:39:24 +0800

    一级分类-按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,38 +1,11 @@
 <script setup>
-import { getCategoryAPI } from "@/apis/category"
-import { ref, onMounted } from "vue"
-import { useRoute,onBeforeRouteUpdate } from "vue-router"
-import { getBannerAPI } from "@/apis/home"
+import { useBanner } from './composables/useBanner'
+import { useCategory } from './composables/useCategory'
 import GoodsItem from "../Home/components/GoodsItem.vue"
 
+const { bannerList } = useBanner()
+const { categoryData } = useCategory()
 
-const categoryData = ref({});
-const route = useRoute();
-//传入分类id，第一次进入to对象为空（没点），故设置默认值
-const getCategory = async (id = route.params.id) => {
-  const res = await getCategoryAPI(id);
-  categoryData.value = res.result;
-}
-
-onMounted(() => getCategory())
-
-// 路由参数变化 分类数据接口重新发送
-// 参数to为目标路由，调用params.id获取目标分类id
-onBeforeRouteUpdate((to)=>{
-  getCategory(to.params.id)
-})
-
-
-//获取banner
-const bannerList = ref([]);
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite: "2",
-  });
-  bannerList.value = res.result;
-};
-
-onMounted(() => getBanner());
 </script>
 
 <template>
