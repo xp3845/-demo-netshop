b0776bd5db04e766d83c60ac681c2f05347ff637
xp2008 <hslzyao@163.com>
2024-07-28 16:23:46 +0800

    二级分类-基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category'
+import { getCategoryFilterAPI,getSubCategoryAPI } from '@/apis/category'
 import { useRoute } from 'vue-router'
 import { ref,onMounted } from 'vue'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 //获取面包屑导航数据
 const categoryData = ref({})
 const route = useRoute()
@@ -12,6 +13,20 @@ const getCategoryData = async() =>{
 
 onMounted(()=>getCategoryData())
 
+//获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId: route.params.id,
+    page:1,
+    pageSize:20,
+    sortField: 'publishTime'
+})
+const getGoodList = async() =>{
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = res.result.items
+}
+
+onMounted(()=>getGoodList())
 </script>
 
 <template>
@@ -32,7 +47,7 @@ onMounted(()=>getCategoryData())
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
-         <!-- 商品列表-->
+         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
     </div>
   </div>
