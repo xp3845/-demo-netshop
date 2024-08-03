0db99e98d9bf73c7f967ee3367d6d78701e087a1
xp2008 <hslzyao@163.com>
2024-08-03 10:06:31 +0800

    登录-401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 //创建axios实例
 const httpInstance = axios.create({
@@ -23,11 +24,18 @@ httpInstance.interceptors.request.use(config => {
   
   // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   //统一错误提示
   ElMessage({
     type:"warning",
     message: e.response.data.message
   })
+  //401 token失效处理
+  //清除本地用户数据，跳转登录页
+  if(e.response.status === 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
     return Promise.reject(e)
   })
 
