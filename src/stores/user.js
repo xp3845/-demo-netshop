a99df3acd34c921992e0344ca81f0e2f753505fb
xp2008 <hslzyao@163.com>
2024-08-03 09:54:15 +0800

    登录-退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,15 @@ export const useUserStore = defineStore('user', ()=> {
         const res = await loginAPI({account,password})
         userInfo.value = res.result
     }
+    //退出时清除用户信息
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
     //以对象格式把state和action函数返回
     return {
         userInfo,
-        getUserInfo
+        getUserInfo,
+        clearUserInfo
     }
 },{
     persist: true //持久化存储
